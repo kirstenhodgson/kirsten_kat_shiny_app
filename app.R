7f927570dc1af45fa2880361da51326813ca3914
kirstenhodgson <kirstenhodgson@bren.ucsb.edu>
2021-03-07 15:38:53 -0800

    Edits: Widgets 1 and 3 (theming, added description and structure to widget tabs); centered photos; made some text edits

--- app.R
+++ app.R
@@ -47,7 +47,8 @@ fire_perimeters <- read_sf(here("data", "fire_perimeters"), layer = "California_
                                  cause == 17 ~ "Volcanic",
                                  cause == 18 ~ "Escaped Prescribed Burn",
                                  cause == 19 ~ "Illegal Alien Campfire",
-                                 TRUE ~ "Not Recorded"))
+                                 TRUE ~ "Not Recorded")) %>% 
+  mutate(year = as.numeric(year)) #I was hoping that this would make the years appear in order in Widget 2 but no luck
 
 fire_cause_counts <- fire_perimeters %>% 
     group_by(cause_label, year) %>% 
@@ -70,24 +71,25 @@ fire <- readJPEG("fire.jpg")
 #Creating the user interface
 ui <- fluidPage(theme = shinytheme("simplex"),
                 
-                navbarPage("California Fire",
+                navbarPage("Fire in California",
                            tabPanel("Welcome!",
                                    mainPanel(
                                      tabsetPanel(
                                        tabPanel("Background",
                                                 p(""),
-                                                img(src = "fire.png", width = 700, height = 500),
-                                                p("Wildfire devestation and impact across the state of California has been an increasingly worseing issue in recent decades in the face of the changing climate.
-                                                  The 10 largest fires in the states history have all occured since 2000. Increasing temperatures, intense drought conditions, strong Santa Ana winds, and adverse affects caused by humans,
-                                                  such as downed powerlines and the now infamous gender reveal party fireworks that sparked the destructive El Dorado Fire burning more than 20,000 acres in 2020, are all contributors to the states growing
-                                                  threat of wildfire."),
-                                                p("This app will explore incidents of fire in all California counties between 2013-2020 using data from Cal Fire and the California Government. 
-                                                  This app will explore the causes of fire across study years, vizualize fire perimeters across all of california for each given year,
+                                                img(src = "fire.png", width = 700, height = 500, style = "display:block; margin-left: auto; margin-right: auto;"),
+                                                p(" "),
+                                                p("Wildfire devastation and impact across the state of California has been an increasingly worsening issue in recent decades in the face of climate change.
+                                                  The 10 largest fires in the state's history have all occured since 2000. Contributors to the growing threat of wildfire in the state include increasing temperatures, 
+                                                  intense drought conditions, strong Santa Ana winds, and adverse effects caused by humans. Examples of relevant human activities are
+                                                  downed powerlines and the now infamous gender reveal party fireworks, which sparked the destructive El Dorado Fire that burned more than 20,000 acres in 2020."),
+                                                p("This app will explore incidents of fire in all California counties between 2013-2020 using data from Cal Fire and the government of California. 
+                                                  It will explore the causes of fire across study years, vizualize fire perimeters across all of california for each given year,
                                                   visualize the number of fires per county given a range of years, and explore the five largest fires by acreage in each county across the study period to gain a better 
                                                   understanding of how fire intensity and quantity has changed in the last decade."),
                                                 p(" "),
                                                 p(" "),
-                                                img(src = "county_map.gif", width = 700),
+                                                img(src = "county_map.gif", width = "80%", height = "80%", style = "display:block; margin-left: auto; margin-right: auto;"),
                                                 p(" "),
                                                 p("Map of California counties. Image source: geology.com")),
                                        tabPanel("Data Sources", 
@@ -104,17 +106,20 @@ ui <- fluidPage(theme = shinytheme("simplex"),
                                                 a(href = "https://data.ca.gov/dataset/ca-geographic-boundaries/resource/b0007416-a325-4777-9295-368ea6b710e6", "Link"),
                                                 p("This dataset contains information from the California Government Database. It contains spatial data for the perimeters of all California Counties.
                                                   We will use this dataset as a background map to plot fire information spatially across California counties."),
-                                                img(src = "paradise.png", width = 700, height = 500),
+                                                img(src = "paradise.png", width = 700, height = 500, style = "display:block; margin-left: auto; margin-right: auto;"),
                                                 p("Paradise, California Destruction after the Camp Fire 2019. Image source: New York Times"),
                                                 a(href = "https://www.nytimes.com/2019/02/28/business/energy-environment/pge-camp-fire.html", "Photo Source")
                                                 ),
                                        tabPanel("How to Use",
+                                                p(" "),
+                                                p(" "),
                                                 p("This app contains widgets that allow the user to explore the number of fires in California by cause, view a map of fire perimeters in each year, view a map of the number of fires per county, and explore the number of acres burned per county."),
                                                 h2("To use this app:"),
-                                                p("1. Select a tab along the top of the app to view the widgets."),
+                                                p(" "),
+                                                p("1. Select a tab along the top of the app to view a widget."),
                                                 p("2. Select a widget input on the left hand side as directed."),
                                                 p("3. View the output created by your selection!"),
-                                                img(src = "thomas.png", width = 700, height = 500),
+                                                img(src = "thomas.png", width = 700, height = 500, style = "display:block; margin-left: auto; margin-right: auto;"),
                                                 p("View of Santa Barbara During the Thomas Fire in 2017. Image Source: Santa Barbara Independent."),
                                                 a(href = "https://www.independent.com/2017/12/13/closing-schools-and-moving-finals-due-thomas-fire/", "Photo Source"))
                                      )
@@ -127,9 +132,15 @@ ui <- fluidPage(theme = shinytheme("simplex"),
                                                                         choices = unique(fire_perimeters$cause_label),
                                                                         selected = "Unknown/Unidentified")
                                         ),
-                                        mainPanel(plotOutput("sw_plot"),
-                                                  "This is where I am going to write and format a description of the graph of fire cause.
-                                                  Information about fire cause classification was taken from the web-based data source.")
+                                        mainPanel(h3("Changes in fire cause through time"),
+                                                  p(" "),
+                                                  p("This widget charts the number of fires in California per year, grouped by the selected fire cause. You can
+                                                  select as many possible causes as you would like to explore trends over time"),
+                                                  plotOutput("sw_plot"),
+                                                  p(" "),
+                                                  p("Information about fire cause classification was taken from the website of the data source:"),
+                                                  a(href = "https://gis.data.ca.gov/datasets/CALFIRE-Forestry::california-fire-perimeters-all?geometry=-138.776%2C31.410%2C-99.445%2C43.564", "CALFIRE Perimeter Data"),
+                                                  )
                                     )
                            ),
                            tabPanel("Widget 2",
@@ -156,12 +167,22 @@ ui <- fluidPage(theme = shinytheme("simplex"),
                                                      sliderInput(inputId = "choose_years",
                                                                  label = "Choose a range of fire years:",
                                                                  min = 2013,
-                                                                 max = 2020,
-                                                                 value = c(2013,2020),
+                                                                 max = 2019,
+                                                                 value = c(2013,2019),
                                                                  sep = "")
                                                      ),
-                                        mainPanel(plotOutput("sw_plot_3"),
-                                                  "This is where I am going to write and format an awesome description of my map of fire counts.")
+                                        mainPanel(h3("Fire frequency by county, 2013-2019"),
+                                                  p(" "),
+                                                  p("This widget creates a chloropleth map of California that shows the number of fires by county
+                                                    in the selected time range. Slide the buttons to the desired year range to see which counties had the
+                                                    most fires in that time."),
+                                                  p(" "),
+                                                  plotOutput("sw_plot_3"),
+                                                  p(" "),
+                                                  p("Lighter colors indicate more fires in the selected year range, while darker colors indicate fewer fires."),
+                                                  p("Imperial County (shown in gray) had no recorded wildfire incidents within our dataset for the years 2013-2019."),
+                                                  p("The data for this chloropleth map was available from the California Wildfire Perimeters 1950-2019 and California
+                                                    Counties Spatial datasets, listed under the Data Sources tab on the welcome page."))
                                     )
                                     ),
                            tabPanel("Widget 4",
@@ -201,7 +222,25 @@ server <- function(input, output) {
       labs(x = "\nYear", 
            y = "Number of fires\n", 
            color = "Fire cause",
-           title = "Number of California fires by cause, 2013-2019")
+           title = "Number of California fires by cause, 2013-2019") +
+      theme(title = element_text(color = "firebrick4"),
+            axis.text = element_text(color = "firebrick4")) +
+      scale_color_manual(values = c("firebrick4",
+                                    "black",
+                                    "goldenrod3",
+                                    "mediumvioletred",
+                                    "tan4",
+                                    "dodgerblue3",
+                                    "lightseagreen",
+                                    "green4",
+                                    "darkblue",
+                                    "coral3",
+                                    "darkslategray",
+                                    "lightpink1",
+                                    "palegreen3",
+                                    "orchid",
+                                    "cadetblue1",
+                                    "azure4"))
 
   })
   
